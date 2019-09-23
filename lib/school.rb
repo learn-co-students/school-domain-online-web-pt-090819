cc6d3d9de96e6d352f5f754922ed7b7131a55a2b
Cody Dupuis <ctdupuis7@gmail.com>
2019-09-23 16:55:46 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
-# code here!
+class School
+  # ROSTER = {} <= that didn't work as a variable, but @roster did. Why?
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def roster
+    @roster
+  end
+  
+  def add_student(stud_name, stud_grade)
+    if !(@roster.keys.include?(stud_grade))
+      @roster[stud_grade] = []
+      @roster[stud_grade] << stud_name
+    else
+      @roster[stud_grade] << stud_name
+    end
+  end
+  
+  def grade(grade) 
+    @roster[grade]
+  end
+  
+  def sort 
+    @roster.each do |key, value|
+      value.sort!  #what does the bang operator do right here?
+    end
+  end
+end
