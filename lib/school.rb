cfa6c238911e686f3eb2d1537dd5697d6d301117
ALEXANDRA OLIVEROS <alexofc@gmail.com>
2019-09-29 15:32:38 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,37 @@
-# code here!
+require 'pry'
+class School
+  attr_accessor :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(student_name, grade)
+  
+    if !roster[grade]
+      roster[grade] = []
+    end
+    roster[grade] << student_name
+    
+#    if roster[grade]
+#      roster[grade] << student_name
+#    else
+#      roster[grade] = []
+#      roster[grade] << student_name
+#    end
+  end
+  
+  def grade(grade)
+    roster[grade]
+  end
+  
+  def sort
+    roster.each do |grade,student|
+      roster[grade] = student.sort
+    end
+    roster
+  end
+  
+  
+end
